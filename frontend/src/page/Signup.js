ab5ee4a0777b828ce426353044e94f22e71016a0
rajpatil2006 <rajvpatil2006@gmail.com>
2025-12-07 20:10:06 +0530

    Update signup URL to production endpoint

--- frontend/src/page/Signup.js
+++ frontend/src/page/Signup.js
@@ -32,7 +32,7 @@ function Signup() {
     }
 
     try{
-        const url= "http://localhost:5000/auth/signup";
+        const url= "https://login-tutorial-mern.vercel.app/auth/signup";
         const response = await fetch(url,
             {
                 method:"POST",
